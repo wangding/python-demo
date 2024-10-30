d6c75662957ebf4c97f7defa41510bc547be90bb
wangding <408542507@qq.com>
2024-10-30 14:25:06 +0800

    08-vars ok

---
+++ 01-pop/01-start/08-vars.py
@@ -0,0 +1,72 @@
+# 整型
+age = 25
+
+# 浮点型
+height = 5.9
+
+# 复数
+complex_num = 3 + 4j;
+
+# 布尔类型
+is_student = True
+
+# 字符串
+name = "Alice"
+
+# 列表
+fruits = ["apple", "banana", "cherry"]
+
+# 元组
+coordinates = (10, 20)
+
+# 范围
+numbers = range(1, 5)
+
+# 集合
+unique_numbers = {1, 2, 3, 4, 5}
+
+# 不可变集合
+immutable_set = frozenset({1, 2, 3})
+
+# 字典类型
+person = {"name": "Alice", "age": 25}
+
+# 字节序列
+byte_sequence = b'binary data'
+
+# 可变字节序列
+byte_array = bytearray(b'mutable binary data')
+
+# 内存视图 
+memory_view = memoryview(byte_array)
+
+# None 类型
+value = None
+
+# 类型注解
+age: int = 26
+height: float = 5.9
+name: str = "Alice"
+is_student: bool = True
+fruits: list = ["apple", "banana", "cherry"]
+coordinates: tuple = (10, 20)
+person: dict = {"name": "Alice", "age": 25}
+unique_numbers: set = {1, 2, 3, 4, 5}
+value: None = None
+
+# 打印变量
+print(age)
+print(height)
+print(complex_num)
+print(is_student)
+print(name)
+print(fruits)
+print(coordinates)
+print(range)
+print(unique_numbers)
+print(immutable_set)
+print(person)
+print(byte_sequence)
+print(byte_array)
+print(memory_view)
+print(value)
