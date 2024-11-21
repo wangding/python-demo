3cd3ac6ea2d3721d8907ceffeb6491da551974f9
wangding <408542507@qq.com>
2024-11-21 14:42:44 +0800

    08_company/updatedb ok

---
+++ 02_oop/08_company/updatedb.py
@@ -0,0 +1,15 @@
+import shelve
+
+db = shelve.open('persondb') # Reopen shelve with same filename
+
+for key in sorted(db): # Iterate to display database objects
+  print(key, '\t=>', db[key]) # Prints with custom format
+
+sue = db['Sue Jones'] # Index by key to fetch
+sue.giveRaise(.10) # Update in memory using class's method
+db['Sue Jones'] = sue # Assign to key to update in shelve
+
+for key in sorted(db): # Iterate to display database objects
+  print(key, '\t=>', db[key]) # Prints with custom format
+
+db.close() # Close after making changes
