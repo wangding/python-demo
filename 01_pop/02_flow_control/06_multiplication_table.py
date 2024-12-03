24c675220d9645d2b7186c7ef8e6568f19b64697
wangding <408542507@qq.com>
2024-12-03 14:41:20 +0800

    modify 06_multiplication_table

--- 01_pop/02_flow_control/06_multiplication_table.py
+++ 01_pop/02_flow_control/06_multiplication_table.py
@@ -1,4 +1,9 @@
-for n in range(1, 10):
-    for m in range(1, n+1):
-        print(f'{m}×{n}={m*n}', end=' ')
-    print('')
+total = int(input('Please input n[1-9]: '))
+for n in range(1, total+1):
+  for m in range(1, n+1):
+    exp = f'{m}×{n}={m*n}'
+    if m==2 and (n==3 or n==4):
+      print(f'{exp:6}', end=' ')
+    else:
+      print(exp, end=' ')
+  print('')
